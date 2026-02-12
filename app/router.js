6c1ca726f5dccbb045bd57acb6ede5c396a75991
klmabeke99 <klmabeke99@gmail.com>
2026-02-12 13:55:54 +0000

    Update router.js

--- app/router.js
+++ app/router.js
@@ -1,29 +1,28 @@
 import { renderStartScreen } from "../screens/start.js";
+import { renderQuestionsScreen } from "../screens/questions.js";
 
 function getRoute() {
-  // Default route
-  const hash = window.location.hash || "#/start";
-  return hash;
+  return window.location.hash || "#/start";
 }
 
 function renderRoute() {
   const route = getRoute();
 
-  // For now, we only have one screen: start
   if (route === "#/start") {
     renderStartScreen();
     return;
   }
 
-  // Fallback if user types a wrong URL
+  if (route === "#/questions") {
+    renderQuestionsScreen();
+    return;
+  }
+
+  // fallback
   window.location.hash = "#/start";
 }
 
 export function initRouter() {
-  // Render when the page loads
   renderRoute();
-
-  // Render when the hash changes
   window.addEventListener("hashchange", renderRoute);
 }
-
