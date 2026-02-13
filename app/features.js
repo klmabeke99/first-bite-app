44a4f83d7e8ff168f049c7e9ee46b9f7dfd421c2
klmabeke99 <klmabeke99@gmail.com>
2026-02-13 17:33:52 +0000

    Update features.js

--- app/features.js
+++ app/features.js
@@ -1,1 +1,15 @@
+import { getState, setState } from "./state.js";
 
+// Simple local premium flag (later can be replaced with license validation)
+export function isPremium() {
+  const { premiumUnlocked } = getState();
+  return !!premiumUnlocked;
+}
+
+export function unlockPremium() {
+  setState({ premiumUnlocked: true });
+}
+
+export function lockPremium() {
+  setState({ premiumUnlocked: false });
+}
